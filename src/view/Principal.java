cd79a081bfd43cbe5b824ff5e4312db1cefbf534
Renan de Queiroz Silva <renanqsilva@gmail.com>
2026-02-24 17:40:32 -0300

    Update Principal.java

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -4,14 +4,14 @@ import javax.swing.JOptionPane;
 public class Principal {
     public static void main(String[] args){
         Metodos m = new Metodos();
-        int n = Integer.parseInt(JOptionPane.showInputDialog(""));
-        if(n % 2 == 0){
-            while(n % 2 == 0){
-             n = Integer.parseInt(JOptionPane.showInputDialog(""));         
-            }
+        int n;
+        do{
+           n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número ímpar:"));
+        }while(n % 2 == 0);
         }
         int cont = 1;
         int resultado = 1;
         System.out.println(m.DuploFatorial(n, cont, resultado));
     }
 }
+
